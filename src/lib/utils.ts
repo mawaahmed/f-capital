03ef02608c7f6ee004838e45b351372a6f504906
mawa ahmed <ahmedmawa8@gmail.com>
2025-10-02 12:02:29 +0100

    resolution des problemes de bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import type { NotificationType } from "@/types/common";
+// import type { NotificationType } from "@/types/common";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -13,21 +13,21 @@ export function capitalizeFirstLetter(text: string): string {
   return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
 
-export function getNotificationTitleFromType(
-  notType: NotificationType
-): string {
-  switch (notType) {
-    case "SUPPLIERORDERCREATED":
-      return "New created order";
-    case "RETAILERORDERCREATED":
-      return "New retailer order";
-    case "SUPPLIERUPDATEDORDER":
-      return "Order updated";
-    case "ORDERUPDATED":
-      return "Order updated";
-    case "RETAILERUPDATEDORDER":
-      return "Retailer order update";
-    default:
-      return "New message";
-  }
-}
+// export function getNotificationTitleFromType(
+//   notType: NotificationType
+// ): string {
+//   switch (notType) {
+//     case "SUPPLIERORDERCREATED":
+//       return "New created order";
+//     case "RETAILERORDERCREATED":
+//       return "New retailer order";
+//     case "SUPPLIERUPDATEDORDER":
+//       return "Order updated";
+//     case "ORDERUPDATED":
+//       return "Order updated";
+//     case "RETAILERUPDATEDORDER":
+//       return "Retailer order update";
+//     default:
+//       return "New message";
+//   }
+// }
